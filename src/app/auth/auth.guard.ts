2d22317dc115c772a1664c2001906c858db5a477
Simon Unterlugauer <simonunterlugauer@LaptopvonSimon.fritz.box>
2024-04-15 09:08:48 +0200

    updated auth guard with rerouting

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,10 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivateFn } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { map, tap } from 'rxjs/operators';
+import { inject } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +12,17 @@ import { AuthService } from '../services/auth.service';
 export class PermissionService {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate() {
-    const authState = this.authService.getAuthState();
-
-    console.log(authState);
-
-    if (!authState) {
-      this.router.navigate(["/views/login"]);
-      console.log("hallo");
-    }
-
-    return authState;
+  canActivate(): Observable<boolean> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.getAuthState().pipe(
+      tap(authState => {
+        console.log(authState); 
+        if (!authState) {
+          this.router.navigate(["/views/login"]);
+          console.log("Nicht authentifiziert, umleiten...");
+        }
+      }),
+      map(authState => !!authState) 
+    );
   }
 }
 export const authGuard: CanActivateFn = (route, state) => {
